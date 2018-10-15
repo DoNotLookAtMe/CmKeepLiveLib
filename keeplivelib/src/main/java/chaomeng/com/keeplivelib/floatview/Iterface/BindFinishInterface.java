caf31e5729a217a8e15942d08b9d17dcf2653ca1
SQSong <SQSong66@163.com>
2018-10-15 21:45:13 +0800

    lib提交

---
+++ keeplivelib/src/main/java/chaomeng/com/keeplivelib/floatview/Iterface/BindFinishInterface.java
@@ -0,0 +1,10 @@
+package chaomeng.com.keeplivelib.floatview.Iterface;
+
+/**
+ * Author: SQSong
+ * Date: 2018/10/11
+ * Description:
+ */
+public interface BindFinishInterface {
+    void bindFinishListen();
+}
